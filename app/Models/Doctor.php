5afea2f41c8fd1d60ff3d8d72fe7466cf658c606
StevenTiu22 <steventiu22@gmail.com>
2025-01-17 06:29:07 +0800

    Added attributes and defined relationships with User, Barangay Official, and Doctor model

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -2,14 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Doctor extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'user_id',
@@ -24,6 +26,30 @@ class Doctor extends Model
 
     public function specializations() : BelongsToMany
     {
-        return $this->belongsToMany(Specialization::class, $table="doctor_has_specialization");
+        return $this->belongsToMany(Specialization::class, $table="doctor_specialization");
+    }
+
+    // Accessors and mutators
+
+    /**
+     * Get the total number of doctors.
+     * @return Attribute
+     */
+    protected function totalDoctors() : Attribute
+    {
+        return Attribute::make(
+            get: fn () => self::query()->count(),
+        );
+    }
+
+    /**
+     * Get the total number of specializations associated with the doctor.
+     * @return Attribute
+     */
+    protected function totalSpecializations() : Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->specializations()->count(),
+        );
     }
 }
