fd866bfba01e5db46e7b52428428c08a97242fcd
StevenTiu22 <steventiu22@gmail.com>
2025-01-31 17:00:09 +0800

    Feat(email): implemented email using MailGun

---
+++ resources/views/emails/test-email.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html >
+<html >
+<head >
+    <title > Example Email </title >
+</head >
+<body >
+    <h1 > Hello, Testing!</h1>
+<p>Thank you for being a valued member of our community.</p>
+
+<p>If you have any questions or need assistance, feel free to reach out to us at <a
+        href="mailto:support@communihealthplus.com">support@communihealthplus.com</a>.</p>
+
+<p>Best regards,</p>
+<p>The CommuniHealth+ Team</p>
+</body>
+</html>
