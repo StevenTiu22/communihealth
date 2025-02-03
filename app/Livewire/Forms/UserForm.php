4644b56d8a9cc166ac3ec322e30249f22c0ac87f
StevenTiu22 <steventiu22@gmail.com>
2025-02-03 16:22:11 +0800

    Feat(users): added a form for Users
    This form will allow the validation for re-use. It also clears the complex code in Livewire component Add

--- app/Livewire/Forms/UserForm.php
+++ app/Livewire/Forms/UserForm.php
@@ -7,5 +7,54 @@ use Livewire\Form;
 
 class UserForm extends Form
 {
-    //
+    #[Validate([
+        'first_name' => 'required|string|max:255',
+        'middle_name' => 'nullable|string|max:255',
+        'last_name' => 'required|string|max:255',
+        'birth_date' => 'required|date',
+        'sex' => 'required|in:0,1',
+        'contact_no' => ['required', 'string', 'regex:/^\+63\d{10}$/'],
+        'email' => 'required|email|unique:users,email',
+        'username' => 'required|string|max:20|unique:users,username',
+        'password' => 'required|string|min:8',
+        'profile_photo_path' => 'nullable|string',
+    ], message: [
+        'first_name.required' => "The first name field is required.",
+        'last_name.required' => "The last name field is required.",
+        'birth_date.required' => "The birth date field is required.",
+        'birth_date.date' => "The birth date must be a valid date.",
+        'sex.required' => "The sex field is required.",
+        'sex.in' => "The selected sex is invalid.",
+        'contact_no.required' => "The contact number field is required.",
+        'contact_no.regex' => "The contact number format must be +63XXXXXXXXXX.",
+        'email.required' => "The email field is required.",
+        'email.email' => "The email must be a valid email address.",
+        'email.unique' => "The email has already been taken.",
+        'username.required' => "The username field is required.",
+        'username.max' => "The username must not exceed 20 characters.",
+        'username.unique' => "The username has already been taken.",
+        'password.required' => "The password field is required.",
+        'password.min' => "The password must be at least 8 characters.",
+    ], attribute: [
+        'first_name' => 'First Name',
+        'middle_name' => 'Middle Name',
+        'last_name' => 'Last Name',
+        'birth_date' => 'Birth Date',
+        'sex' => 'Sex',
+        'contact_no' => 'Contact Number',
+        'email' => 'Email Address',
+        'username' => 'Username',
+        'password' => 'Password',
+        'profile_photo_path' => 'Profile Photo',
+    ])]
+    public string $first_name = '';
+    public string $middle_name = '';
+    public string $last_name = '';
+    public string $birth_date = '';
+    public string $sex = '';
+    public string $contact_no = '';
+    public string $email = '';
+    public string $username = '';
+    public string $password = '';
+    public string $profile_photo_path = '';
 }
--- app/Livewire/Users/Add.php
+++ app/Livewire/Users/Add.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Users;
 
+use App\Livewire\Forms\UserForm;
 use App\Models\Address;
 use App\Models\ParentInfo;
 use App\Models\Specialization;
@@ -15,132 +16,19 @@ class Add extends Component
 {
     use WithFileUploads;
 
-    public $role = -1;
-    public $profile_photo;
-    public $first_name = '';
-    public $middle_name = '';
-    public $last_name = '';
-    public $birth_date = '';
-    public $gender = '';
-    public $father_first_name = '';
-    public $father_middle_name = '';
-    public $father_last_name = '';
-    public $father_philhealth = '';
-    public $mother_first_name = '';
-    public $mother_middle_name = '';
-    public $mother_last_name = '';
-    public $mother_philhealth = '';
-    public $password = '';
-    public $password_confirmation = '';
-    public $house_number = '';
-    public $street = '';
-    public $barangay = '';
-    public $city = '';
-    public $license_number = '';
-    public $specialization = '';
-    public $local_government_unit = '';
-    public $issuance_date = '';
-    public $position = '';
-    public $term_start = '';
-    public $term_end = '';
-
-    protected function rules()
-    {
-        return [
-            'first_name' => ['required', 'string', 'max:255', 'regex:/^[a-zA-Z\s\.]+$/'],
-            'middle_name' => ['nullable', 'string', 'max:255', 'regex:/^[a-zA-Z\s\.]+$/'],
-            'last_name' => ['required', 'string', 'max:255', 'regex:/^[a-zA-Z\s\.]+$/'],
-            'birth_date' => ['required', 'date', 'before:today'],
-            'gender' => ['required', 'in:0,1'],
-            'father_first_name' => ['required', 'string', 'max:255', 'regex:/^[a-zA-Z\s\.]+$/'],
-            'father_middle_name' => ['nullable', 'string', 'max:255', 'regex:/^[a-zA-Z\s\.]+$/'],
-            'father_last_name' => ['required', 'string', 'max:255', 'regex:/^[a-zA-Z\s\.]+$/'],
-            'father_philhealth' => ['required', 'string', 'digits:12'],
-            'mother_first_name' => ['required', 'string', 'max:255', 'regex:/^[a-zA-Z\s\.]+$/'],
-            'mother_middle_name' => ['nullable', 'string', 'max:255', 'regex:/^[a-zA-Z\s\.]+$/'],
-            'mother_last_name' => ['required', 'string', 'max:255', 'regex:/^[a-zA-Z\s\.]+$/'],
-            'mother_philhealth' => ['required', 'string', 'digits:12'],
-            'password' => ['required', 'string', 'min:8', 'regex:/^\S*$/'],
-            'password_confirmation' => ['required_with:password', 'same:password'],
-            'role' => ['required', 'in:0,1,2,3'],
-            'profile_photo' => ['nullable', 'image', 'max:1024'],
-            'house_number' => ['required', 'string', 'max:50'],
-            'street' => ['required', 'string', 'max:255'],
-            'barangay' => ['required', 'string', 'max:255'],
-            'city' => ['required', 'string', 'max:255'],
-            'license_number' => ['required_if:role,2', 'string', 'max:50'],
-            'specialization' => ['required_if:role,2', 'string', 'max:100'],
-            'local_government_unit' => ['required_if:role,1', 'string', 'max:255'],
-            'issuance_date' => ['required_if:role,1', 'date', 'before_or_equal:today'],
-            'position' => ['required_if:role,0', 'string', 'max:255'],
-            'term_start' => ['required_if:role,0', 'date'],
-            'term_end' => ['required_if:role,0', 'date', 'after:term_start', 'after:today'],
-        ];
-    }
-
-    protected function messages()
-    {
-        return [
-            'first_name.required' => 'Please enter your first name.',
-            'first_name.regex' => 'First name can only contain letters, spaces and dots.',
-            'middle_name.regex' => 'Middle name can only contain letters, spaces and dots.',
-            'last_name.required' => 'Please enter your last name.',
-            'last_name.regex' => 'Last name can only contain letters, spaces and dots.',
-            'birth_date.required' => 'Please enter your birth date.',
-            'birth_date.before' => 'Birth date must be a date before today.',
-            'gender.required' => 'Please select your gender.',
-            'gender.in' => 'Please select a valid gender.',
-            'father_first_name.required' => 'Please enter father\'s first name.',
-            'father_first_name.regex' => 'Father\'s first name can only contain letters, spaces and dots.',
-            'father_middle_name.regex' => 'Father\'s middle name can only contain letters, spaces and dots.',
-            'father_last_name.required' => 'Please enter father\'s last name.',
-            'father_last_name.regex' => 'Father\'s last name can only contain letters, spaces and dots.',
-            'father_philhealth.required' => 'Please enter father\'s PhilHealth number.',
-            'father_philhealth.size' => 'PhilHealth number must be 12 digits.',
-            'mother_first_name.required' => 'Please enter mother\'s first name.',
-            'mother_first_name.regex' => 'Mother\'s first name can only contain letters, spaces and dots.',
-            'mother_middle_name.regex' => 'Mother\'s middle name can only contain letters, spaces and dots.',
-            'mother_last_name.required' => 'Please enter mother\'s last name.',
-            'mother_last_name.regex' => 'Mother\'s last name can only contain letters, spaces and dots.',
-            'mother_philhealth.required' => 'Please enter mother\'s PhilHealth number.',
-            'mother_philhealth.size' => 'PhilHealth number must be 12 digits.',
-            'password.required' => 'Please enter a password.',
-            'password.min' => 'Password must be at least 8 characters.',
-            'password.regex' => 'Password cannot contain spaces.',
-            'password_confirmation.same' => 'The password confirmation does not match.',
-            'password_confirmation.required_with' => 'Please confirm your password.',
-            'role.required' => 'Please select a role.',
-            'role.in' => 'Please select a valid role.',
-            'profile_photo.image' => 'The file must be an image.',
-            'profile_photo.max' => 'The image must not be larger than 1MB.',
-            'house_number.required' => 'Please enter your house number.',
-            'street.required' => 'Please enter your street.',
-            'barangay.required' => 'Please enter your barangay.',
-            'city.required' => 'Please enter your city.',
-            'license_number.required_if' => 'Please enter your license number.',
-            'specialization.required_if' => 'Please enter your specialization.',
-            'local_government_unit.required_if' => 'Please enter the Local Government Unit.',
-            'issuance_date.required_if' => 'Please enter the issuance date.',
-            'issuance_date.before_or_equal' => 'Issuance date cannot be in the future.',
-            'position.required_if' => 'Please select a position.',
-            'term_start.required_if' => 'Please enter the term start date.',
-            'term_end.required_if' => 'Please enter the term end date.',
-            'term_end.after' => 'Term end date must be after term start date and today.'
-        ];
-    }
-
+    public ?UserForm $form;
 
     public $showModal = false;
 
-    public function openModal()
+    public function open()
     {
         $this->showModal = true;
     }
 
-    public function closeModal()
+    public function close()
     {
         $this->showModal = false;
-        $this->reset();
+        $this->form->reset();
     }
 
     public function updated($propertyName)
